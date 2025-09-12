a86e896baaab9c46d660c4ab986cfbb9daf6e383
Mihai Hodis <mihaihodis98@gmail.com>
2025-09-12 11:21:13 +0300

    finalizare endpoint outsideWeather

--- routes/outsideWeatherRoutes.js
+++ routes/outsideWeatherRoutes.js
@@ -1,10 +1,9 @@
-// routes/outsideWeatherRoutes.js
-import { Router } from 'express';
-import { listOutsideWeather } from '../controllers/outsideWeatherController.js';
+import { Router } from "express";
+import { listOutsideWeather } from "../controllers/outsideWeatherController.js";
+import { verifyFirebaseToken } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
-// GET /outside_weather?greenhouse_id=1&from=2025-07-23T00:00:00Z&to=2025-07-24T00:00:00Z&limit=500
-router.get('/', listOutsideWeather);
+router.get("/", verifyFirebaseToken, listOutsideWeather);
 
 export default router;
